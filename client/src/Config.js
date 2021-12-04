1f9b21036402fae64d19a13d1797d38e0bdb6ce8
LAPTOP-2NH5B2GR\Deepanshu yadav <kabirsngh0023@gmail.com>
2021-12-04 11:52:01 +0530

    heading change

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,4 +1,4 @@
 
 export const API_URL = process.env.REACT_APP_MODE ==="development" ? "http://localhost:8000/api/v1" : "/api/v1"
 export const webName = "NITIVet"
-export const MainPageImageHeading = "NITIVet - Telemedicine for Livestock Health and Safety."
+export const MainPageImageHeading = "NITIVet - Telemedicine for Livestock Health and Safety"
